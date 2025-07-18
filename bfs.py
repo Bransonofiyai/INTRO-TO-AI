03dcb1f3bd49ff1804c366168365aa13c1c96e71
Bransonofiyai <ofiyaibranson@gmail.com>
2025-07-18 18:34:34 +0200

    Create bfs.py

---
+++ bfs.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+def bfs(graph, start, goal):
+    visited = set()
+    queue = deque([[start]])
+    while queue:
+        path = queue.popleft()
+        node = path[-1]
+        if node == goal:
+            return path
+        if node not in visited:
+            visited.add(node)
+            for neighbor in graph.get(node, []):
+                new_path = list(path)
+                new_path.append(neighbor)
+                queue.append(new_path)
+    return None
+
+if __name__ == "__main__":
+    graph = {
+        'A': ['B', 'C'],
+        'B': ['D', 'E'],
+        'C': ['F'],
+        'D': [],
+        'E': ['F'],
+        'F': []
+    }
+    print("BFS path from A to F:", bfs(graph, 'A', 'F'))
